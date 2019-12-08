3491b01b0d343632216261d0f3bed76c1ed8882a
Jonathan Bertoldi <contato@jonathanbertoldi.com.br>
2019-12-08 20:27:25 -0300

    break markup into components

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './menu-item.styles.scss';
+
+const MenuItem = ( {title, imageUrl, size} ) => (
+    <div style={{backgroundImage: `url(${imageUrl})` }} className={`${size} menu-item`}>
+        <div className="content">
+            <h2 className="title">{title}</h2>
+            <span className="subtitle">SHOP NOW</span>
+        </div>
+    </div>
+)
+
+export default MenuItem;
